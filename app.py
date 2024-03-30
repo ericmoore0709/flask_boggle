15193c510caf2a11bcef38a13f374f946167e6b9
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2024-03-30 22:01:16 +0000

    Displayed boggle board and added guess form

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
+from uuid import uuid4
 
 boggle_game = Boggle()
 app = Flask(__name__)
 
+# session key
+app.secret_key = str(uuid4())
+
+
 @app.get('/')
 def index():
-    return render_template('index.html')
+    board = session.get('board', default=boggle_game.make_board())
+    session['board'] = board
+    return render_template('index.html', board=board)
