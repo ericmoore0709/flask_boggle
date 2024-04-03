9e43200d2be80107ee17811997fe03c3bcf2924c
Eric Moore <ericmoore0709@gmail.com>
2024-04-03 01:01:27 -0700

    Made API communication

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, jsonify
 from uuid import uuid4
 
 boggle_game = Boggle()
@@ -15,6 +15,30 @@ def index():
     session['board'] = board
     return render_template('index.html', board=board)
 
-@app.post('/guess')
+
+@app.post('/api/guess')
 def guess():
-    pass
+    attempted_guess = request.json.get('guess', '')
+
+    # if the value isn't there, return a data validation error DTO
+    if not attempted_guess:
+        return jsonify({
+            'status': 400,
+            'message': '\'guess\' is required.'
+        })
+
+    # else, see if the guess is a valid word and can be found in the board.
+    board = session.get('board', [])
+    if not board:
+        return jsonify({
+            'status': 404,
+            'message': "Board not found."
+        })
+
+    result = boggle_game.check_valid_word(board, attempted_guess)
+
+    return jsonify({
+        'status': 200,
+        'message': 'Here is the result.',
+        'result': result
+    })
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,26 @@
-$('#guess_form').submit((e) => {
-    e.preventDefault();
-    
-    
+const BASE_URL = 'http://127.0.0.1:5000/' // localhost URL
+// const BASE_URL = '' // codespace URL
 
+$('#guess_form').submit(async (e) => {
+    e.preventDefault();
+
+    const guess = $('#guess_input').val();
+
+    if (!guess)
+        return;
+
+    // make API call?
+    const result = await axios.post(BASE_URL + '/api/guess', JSON.stringify({
+        'guess': guess
+    }), {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((result) => {
+            alert(result.data.result);
+        }).catch((err) => {
+            console.log(err);
+            alert('Error:', err);
+        });
 })
